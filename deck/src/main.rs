dbf5288ba2a21da86088382deee78810992d29a7
roanrichmond <roanrichmond@codethink.co.uk>
2024-11-05 17:41:41 +0000

    deck: Add struct to represent a Card
    This includes adding enums for suits and card numbers.
    Modified deck struct to contain a vec of Cards.

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -2,19 +2,20 @@ use rand::{seq::SliceRandom, thread_rng};
 
 #[derive(Debug)]
 struct Deck {
-    cards: Vec<String>,
+    cards: Vec<Card>,
 }
 
 impl Deck {
     fn new() -> Self {
-        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-        let values = ["Ace", "Two", "Three", "Four"];
         let mut cards = vec![];
 
-        for suit in suits {
-            for value in values {
-                let card = format!("{} of {}", value, suit);
-                cards.push(card)
+        for suit in [Suits::Clubs, Suits::Diamonds, Suits::Hearts, Suits::Spades] {
+            for card_number in 1..14 {
+                let card = Card {
+                    suit,
+                    number: card_number.into(),
+                };
+                cards.push(card);
             }
         }
 
@@ -26,12 +27,63 @@ impl Deck {
         self.cards.shuffle(&mut rng);
     }
 
-    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+    fn deal(&mut self, num_cards: usize) -> Vec<Card> {
         self.cards.split_off(self.cards.len() - num_cards)
     }
 }
 
-// Add your own random number generator
+#[derive(Debug)]
+struct Card {
+    suit: Suits,
+    number: CardNumber,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Suits {
+    Spades,
+    Hearts,
+    Clubs,
+    Diamonds,
+}
+
+#[derive(Debug)]
+enum CardNumber {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Joker,
+}
+
+impl From<usize> for CardNumber {
+    fn from(value: usize) -> Self {
+        match value {
+            1 => CardNumber::Ace,
+            2 => CardNumber::Two,
+            3 => CardNumber::Three,
+            4 => CardNumber::Four,
+            5 => CardNumber::Five,
+            6 => CardNumber::Six,
+            7 => CardNumber::Seven,
+            8 => CardNumber::Eight,
+            9 => CardNumber::Nine,
+            10 => CardNumber::Ten,
+            11 => CardNumber::Jack,
+            12 => CardNumber::Queen,
+            13 => CardNumber::King,
+            _ => CardNumber::Joker,
+        }
+    }
+}
 
 fn main() {
     let mut deck = Deck::new();
