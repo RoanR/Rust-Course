6cbcb477f0cea1602acf277bf6e93f5a64d359de
roanrichmond <roanrichmond@codethink.co.uk>
2024-11-06 12:47:15 +0000

    deck: Add Game struct for one player Blackjack

--- deck/src/blackjack.rs
+++ deck/src/blackjack.rs
@@ -1,5 +1,33 @@
 use crate::cards::{Card, Deck};
 
+pub struct Game {
+    deck: Deck,
+    human: Player,
+    computer: Player,
+    turn: bool,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        println!("Starting new game of BlackJack");
+        let mut deck = Deck::new();
+        println!("Shuffling and Dealing ..... ");
+        deck.shuffle();
+        let computer = Player::new(deck.deal(2));
+        let human = Player::new(deck.deal(2));
+        println!(
+            "Dealers showing: \n\tFacedown Card\n\t{}",
+            computer.hand()[1]
+        );
+        println!("Your cards: \n\t{}\n\t{}", human.hand()[0], human.hand()[1]);
+        Self {
+            deck,
+            human,
+            computer,
+            turn: true,
+        }
+    }
+}
 pub struct Player {
     hand: Vec<Card>,
 }
--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,32 +1,8 @@
-use blackjack::Player;
-use cards::Deck;
+use blackjack::Game;
 
 mod blackjack;
 mod cards;
 
 fn main() {
-    let mut deck = Deck::new();
-    println!("Here's your deck: {}", deck);
-
-    println!("Shuffling ... ");
-    deck.shuffle();
-
-    println!("Dealing ... ");
-    let hand = deck.deal(2);
-
-    println!("Here's your hand:");
-    for card in &hand {
-        println!("\t{}", *card);
-    }
-
-    println!("Creating a new Player");
-    let mut p = Player::new(hand);
-    while !p.bust() {
-        deck.hit(&mut p);
-        println!("\nHitting You!");
-        print!("New hand: ");
-        for card in p.hand() {
-            print!("{}, ", card);
-        }
-    }
+    let game = Game::new();
 }
